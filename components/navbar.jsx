ddfafcd040ff73b7da3fd33cfc115a2b8c7dabe1
mahefatsiresy <mahefatsiresy.n.lucas@gmail.com>
2024-10-08 11:31:13 +0300

    update navbar style

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,17 +1,47 @@
-import style from "./styles/navbar.module.css"
-import Link from "next/link"
+// import style from "./styles/navbar.module.css";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
-export default function Navbar(){
-    return(
-        <>
-            <div className={style.navbarBody}>
-                <li>
-                    <Link href={"/"}>Dashboard</Link>
-                    <Link href={"/edito"}>Edito</Link>
-                    <Link href={"/partenaires"}>Partenaires</Link>
-                    <Link href={"/menu"}>Menu</Link>
-                </li>
-            </div>
-        </>
-    )
+export default function Navbar() {
+  return (
+    <aside className="bg-gray-800 text-white w-64 min-h-screen transition-all duration-300 ease-in-out">
+      <div className="p-4">
+        <h1 className="text-2xl font-semibold">Dashboard</h1>
+      </div>
+      <nav className="mt-4">
+        <Link href={"/"}>
+          <Button
+            variant="ghost"
+            className="w-full justify-start text-white hover:bg-gray-700 hover:text-white"
+          >
+            Dashboard
+          </Button>
+        </Link>
+        <Link href={"/edito"}>
+          <Button
+            variant="ghost"
+            className="w-full justify-start text-white hover:bg-gray-700 hover:text-white"
+          >
+            Edito
+          </Button>
+        </Link>
+        <Link href="partenaires">
+          <Button
+            variant="ghost"
+            className="w-full justify-start text-white hover:bg-gray-700 hover:text-white"
+          >
+            Partenaires
+          </Button>
+        </Link>
+        <Link href="blog">
+          <Button
+            variant="ghost"
+            className="w-full justify-start text-white hover:bg-gray-700 hover:text-white"
+          >
+            Blog
+          </Button>
+        </Link>
+      </nav>
+    </aside>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,5 @@
-import Image from "next/image";
-import Link from "next/link";
-import { Inter } from "next/font/google";
-import client from "@/lib/mongodb";
-//import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
-import Head from "next/head";
 import Navbar from "@/components/navbar";
 import style from "@/pages/styles/home.module.css";
-
-/*type ConnectionStatus = {
-  isConnected: boolean;
-};*/
-
-const inter = Inter({ subsets: ["latin"] });
-
-/*xport const getServerSideProps: GetServerSideProps<
-  ConnectionStatus
-> = async () => {
-  try {
-    await client.connect(); // `await client.connect()` will use the default database passed in the MONGODB_URI
-    return {
-      props: { isConnected: true },
-    };
-  } catch (e) {
-    console.error(e);
-    return {
-      props: { isConnected: false },
-    };
-  }
-};*/
 
 export default function Home() {
   return (
