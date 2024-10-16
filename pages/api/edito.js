ec34508c5fb7e3c66a3539c0bc388cbd54d343c5
mahefatsiresy <mahefatsiresy.n.lucas@gmail.com>
2024-10-16 12:41:12 +0300

    fix delete method for edito API

--- pages/api/edito.js
+++ pages/api/edito.js
@@ -145,7 +145,7 @@ export default async function handler(req, res) {
         return res.status(404).json({ error: "File not found" });
       }
 
-      await fsPromises.unlink(file.filePath);
+      await fsPromises.unlink(path.join(UPLOAD_DIR, file.filePath));
       await collection.deleteOne({ _id: new ObjectId(id) });
 
       return res.status(200).json({ message: "File deleted successfully" });
