4c4a8125c7327451b54957fb0a6a23b2695abe60
mahefatsiresy <mahefatsiresy.n.lucas@gmail.com>
2024-12-27 10:09:52 +0300

    disabled auth for build

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,64 +1,64 @@
-import client from "@/lib/mongodb";
-import { ObjectId } from "mongodb";
-import NextAuth, { AuthOptions } from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import { z } from "zod";
-import { compare } from "bcrypt";
+// import client from "@/lib/mongodb";
+// import { ObjectId } from "mongodb";
+// import NextAuth, { AuthOptions } from "next-auth";
+// import Credentials from "next-auth/providers/credentials";
+// import { z } from "zod";
+// import { compare } from "bcrypt";
 
-type User = {
-  id: ObjectId;
-  username: string;
-  password: string;
-};
+// type User = {
+//   id: ObjectId;
+//   username: string;
+//   password: string;
+// };
 
-async function getUser(username: string): Promise<User | undefined> {
-  try {
-    // Get user from mongo
-    const user = await client.db().collection("users").findOne({ username });
-    if (user) {
-      return { id: user._id, username: user.username, password: user.password };
-    }
-  } catch (error) {
-    console.error("Failed to fetch user:", error);
-    throw new Error("Failed to fetch user.");
-  }
-}
+// async function getUser(username: string): Promise<User | undefined> {
+//   try {
+//     // Get user from mongo
+//     const user = await client.db().collection("users").findOne({ username });
+//     if (user) {
+//       return { id: user._id, username: user.username, password: user.password };
+//     }
+//   } catch (error) {
+//     console.error("Failed to fetch user:", error);
+//     throw new Error("Failed to fetch user.");
+//   }
+// }
 
-export const authOptions: AuthOptions = {
-  pages: {
-    signIn: "/login",
-  },
-  providers: [
-    Credentials({
-      name: "credentials",
-      credentials: {
-        username: { label: "Username", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        // Parse crendential
-        const parsedCredentials = z
-          .object({
-            username: z.string(),
-            password: z.string().min(6),
-          })
-          .safeParse(credentials);
+// export const authOptions: AuthOptions = {
+//   pages: {
+//     signIn: "/login",
+//   },
+//   providers: [
+//     Credentials({
+//       name: "credentials",
+//       credentials: {
+//         username: { label: "Username", type: "text" },
+//         password: { label: "Password", type: "password" },
+//       },
+//       async authorize(credentials) {
+//         // Parse crendential
+//         const parsedCredentials = z
+//           .object({
+//             username: z.string(),
+//             password: z.string().min(6),
+//           })
+//           .safeParse(credentials);
 
-        // Get user
-        if (parsedCredentials.success) {
-          const { username, password } = parsedCredentials.data;
-          const user = await getUser(username);
-          if (!user) return null;
-          const passwordMatch = await compare(password, user.password);
+//         // Get user
+//         if (parsedCredentials.success) {
+//           const { username, password } = parsedCredentials.data;
+//           const user = await getUser(username);
+//           if (!user) return null;
+//           const passwordMatch = await compare(password, user.password);
 
-          if (passwordMatch) return user;
-        }
+//           if (passwordMatch) return user;
+//         }
 
-        console.log("Invalid credentials");
-        return null;
-      },
-    }),
-  ],
-};
+//         console.log("Invalid credentials");
+//         return null;
+//       },
+//     }),
+//   ],
+// };
 
-export default NextAuth(authOptions);
+// export default NextAuth(authOptions);
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,91 +1,91 @@
-import { InferGetServerSidePropsType } from "next";
-import { getCsrfToken } from "next-auth/react";
-import { CtxOrReq } from "next-auth/client/_utils";
-import { authOptions } from "./api/auth/[...nextauth]";
-import { getServerSession } from "next-auth";
+// import { InferGetServerSidePropsType } from "next";
+// import { getCsrfToken } from "next-auth/react";
+// import { CtxOrReq } from "next-auth/client/_utils";
+// import { authOptions } from "./api/auth/[...nextauth]";
+// import { getServerSession } from "next-auth";
 
-export default function LoginPage({
-  csrfToken,
-  query,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  return (
-    <div className="flex h-screen flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Se connecter
-        </h2>
-      </div>
+// export default function LoginPage({
+//   csrfToken,
+//   query,
+// }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+//   return (
+//     <div className="flex h-screen flex-col justify-center px-6 py-12 lg:px-8">
+//       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+//         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+//           Se connecter
+//         </h2>
+//       </div>
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        {/*Display error if exist*/}
-        {query.error && query.error === "CredentialsSignin" && (
-          <div className="text-white bg-red-500 p-2 rounded-md">
-            Credentials Error
-          </div>
-        )}
+//       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+//         {/*Display error if exist*/}
+//         {query.error && query.error === "CredentialsSignin" && (
+//           <div className="text-white bg-red-500 p-2 rounded-md">
+//             Credentials Error
+//           </div>
+//         )}
 
-        <form
-          method="post"
-          action="/api/auth/callback/credentials"
-          className="space-y-6"
-        >
-          <input type="hidden" name="csrfToken" defaultValue={csrfToken} />
-          <div>
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Username
-            </label>
-            <div className="mt-2">
-              <input
-                id="username"
-                name="username"
-                type="text"
-                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
+//         <form
+//           method="post"
+//           action="/api/auth/callback/credentials"
+//           className="space-y-6"
+//         >
+//           <input type="hidden" name="csrfToken" defaultValue={csrfToken} />
+//           <div>
+//             <label
+//               htmlFor="username"
+//               className="block text-sm font-medium leading-6 text-gray-900"
+//             >
+//               Username
+//             </label>
+//             <div className="mt-2">
+//               <input
+//                 id="username"
+//                 name="username"
+//                 type="text"
+//                 className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+//               />
+//             </div>
+//           </div>
 
-          <div>
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Password
-              </label>
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                required
-                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
+//           <div>
+//             <div className="flex items-center justify-between">
+//               <label
+//                 htmlFor="password"
+//                 className="block text-sm font-medium leading-6 text-gray-900"
+//               >
+//                 Password
+//               </label>
+//             </div>
+//             <div className="mt-2">
+//               <input
+//                 id="password"
+//                 name="password"
+//                 type="password"
+//                 required
+//                 className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+//               />
+//             </div>
+//           </div>
 
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Sign in
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
-}
+//           <div>
+//             <button
+//               type="submit"
+//               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+//             >
+//               Sign in
+//             </button>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
 
-export async function getServerSideProps(context: any) {
-  return {
-    props: {
-      csrfToken: await getCsrfToken(context as CtxOrReq),
-      query: context.query, // get query for displaying error
-    },
-  };
-}
+// export async function getServerSideProps(context: any) {
+//   return {
+//     props: {
+//       csrfToken: await getCsrfToken(context as CtxOrReq),
+//       query: context.query, // get query for displaying error
+//     },
+//   };
+// }
